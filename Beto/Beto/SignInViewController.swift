2395b9ffc92251a43912b3e35f1d821b32993474
Varun Patel <varun@verve3.io>
2015-11-23 23:21:39 -0500

    iPhone 4S Bug Fix
    iPhone 4S Keyboard Covering Password Field Bug Fix

--- Beto/Beto/SignInViewController.swift
+++ Beto/Beto/SignInViewController.swift
@@ -10,28 +10,27 @@ import UIKit
 import Parse
 
 class SignInViewController: UIViewController {
-
+    
     @IBOutlet weak var signInView: UIView!
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var logInButton: UIButton!
     @IBOutlet weak var invalidLogIn: UILabel!
+    @IBOutlet weak var centerHorizontally: NSLayoutConstraint!
     
     var signInPageTextFields : [UITextField]?
     var signInPageButtons : [UIButton]?
-    var originalSignInViewFrameCenter : CGPoint?
-
+    var keyboardIsVisible = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         signInPageTextFields = [username, password]
         signInPageButtons = [logInButton]
         SignInViewControllerHelper.setAllSignInPageTextFieldBorderWidth(signInPageTextFields!, borderWidth: 1.0)
-        originalSignInViewFrameCenter = signInView.center
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
-
     }
     
     override func viewWillDisappear(animated: Bool) {
@@ -57,21 +56,30 @@ class SignInViewController: UIViewController {
     }
     
     func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
-            UIView.animateWithDuration(0.25, delay: 0.25, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
-                let x = self.logInButton.convertPoint(self.logInButton.center, fromView: self.view)
-                self.signInView.center.y -= keyboardSize.height
-                for views in self.signInView.subviews {
-                    views.center.y -= 40.0
+        if !keyboardIsVisible{
+            if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+                self.keyboardIsVisible = true
+                if ((signInView.center.y + self.signInView.frame.height / 2) >  (self.view.frame.height - keyboardSize.height)) {
+                    let centerOfRootView = self.signInView.center
+                    UIView.animateWithDuration(0.25, delay: 0.25, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+                        self.centerHorizontally.constant -= ((centerOfRootView.y + self.signInView.frame.height / 2) - (self.view.frame.height - keyboardSize.height))
+                        }, completion: nil)
                 }
-                }, completion: nil)
-            
+            }
         }
     }
     
     func keyboardWillHide(notification: NSNotification) {
-        if let _ = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
-            // self.signInView.frame = originalSignInViewFrame!
+        if keyboardIsVisible {
+            if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+                if ((signInView.center.y + self.signInView.frame.height / 2) >  (self.view.frame.height - keyboardSize.height)) {
+                    let centerOfRootView = self.signInView.center
+                    UIView.animateWithDuration(0.25, delay: 0.25, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+                        self.centerHorizontally.constant += ((centerOfRootView.y + self.signInView.frame.height / 2) - (self.view.frame.height - keyboardSize.height))
+                        }, completion: nil)
+                }
+            }
         }
+        keyboardIsVisible = false
     }
 }
