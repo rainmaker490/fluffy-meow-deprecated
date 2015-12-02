3db520bf29456dc94e3b76c44514a847f7641c36
Varun Patel <varun@verve3.io>
2015-12-02 17:24:17 -0500

    Add TableViews to Trending and More
    Add UITableView  to Trending and More View Controllers

---
+++ Beto/Beto/TrendingTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  OffsetTrendingTableViewCell.swift
+//  Beto
+//
+//  Created by Varun D Patel on 12/2/15.
+//  Copyright © 2015 Varun D Patel. All rights reserved.
+//
+
+import UIKit
+
+class TrendingTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
