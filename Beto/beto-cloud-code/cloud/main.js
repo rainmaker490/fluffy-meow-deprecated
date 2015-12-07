e447f2f4cb191bf58e892e6aa81d263affe299e3
Varun Patel <varun@verve3.io>
2015-12-06 22:59:14 -0500

    Cloud Code Parse

---
+++ Beto/beto-cloud-code/cloud/main.js
@@ -0,0 +1,10 @@
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("Hello world!");
+});
+
+Parse.Cloud.job("test", function(request, status){
+  status.success("success!");
+});
